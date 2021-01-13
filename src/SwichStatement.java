3566443f918aadfc742dfe85417844063639cd98
Safrizal <axrizal092@gmail.com>
2021-01-13 12:01:23 +0700

    Add new

--- src/SwichStatement.java
+++ src/SwichStatement.java
@@ -1,7 +1,7 @@
 public class SwichStatement {
     public static void main(String[] args) {
 
-
+// Percabangan yang sama dengan if, switch statement hanya untuk perbandingan ==
         var nilai = "D";
 
         switch (nilai){
